f423568a0487e2030393bce45c071a86836fd490
Aaron Kaspar <aaron.c.kaspar@gmail.com>
2024-07-14 19:02:08 +0200

    add wind_points

--- R/load/config.R
+++ R/load/config.R
@@ -2,8 +2,9 @@ library(here)
 library(dotenv)
 library(dplyr)
 
-# Get password from.env
-# TODO: insert important info & change password in .env
-dotenv::load_dot_env()
-password = Sys.getenv("my_password")
+# # Get password from.env
+# # TODO: insert important info & change password in .env
+# dotenv::load_dot_env()
+# password = Sys.getenv("my_password")
 
+# here::i_am("GIS_OnshoreWind_LocalPolitics/R/load/config.R")
