9915021125a8286875a569e4406d61fb7f4357a6
Aaron Kaspar <aaron.c.kaspar@gmail.com>
2024-07-17 12:07:48 +0200

    Remove files from LFS and track with normal Git

--- R/load/data_read.R
+++ R/load/data_read.R
@@ -1,3 +1,80 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:aa89975b749051fde257790f90a56e95a9097f3a694c42739b50cc8021d9831d
-size 1905
+library(here)
+library(sf)
+library(ggplot2)
+library(sp)
+library(raster)
+library(terra)
+library(ncdf4)
+library(readxl)
+
+## VECTOR DATA
+
+# wind farms as point layer
+wind_points_old <- st_read(
+  here(
+    "data",
+    "input",
+    "Windkraftanlagen DE - Windkraftanlagen 2023",
+    "Windkraftanlagen DE - Windkraftanlagen 2023.shp"
+  )
+)
+st_crs(wind_points_old)
+plot(wind_points_old)
+
+# wind farm database with more recent data
+wind_points <- read_excel(here("data", "input", "Global-Wind-Power-Tracker-June-2024.xlsx"),
+                           sheet = "Data")
+
+# Germany ...TODO: Which level do I want Germany shp?
+deu <- st_read(here("data", "input", "gadm41_DEU_shp", "gadm41_DEU_2.shp"))
+st_crs(deu)
+ggplot() +
+  geom_sf(data = deu) +
+  geom_sf(data = wind_points_old, aes(color = 'red'))
+theme_minimal()
+
+# municipalities for the 2021 federal election
+municipality <- st_read(
+  here(
+    "data",
+    "input",
+    "2021-bundestagswahl-gemeinden",
+    "2021-bundestagswahl-gemeinden.shp"
+  )
+)
+st_crs(municipality)
+ggplot() +
+  geom_sf(data = municipality) +
+  theme_minimal()
+
+# federal election 2021 results
+election_2021 <- read.csv2(
+  here(
+    "data",
+    "input",
+    "2021-bundestagswahl-gemeinden",
+    "2021-bundestagswahl-gemeinden.csv"
+  ),
+  sep = ','
+)
+
+
+## RASTER DATA
+
+# land use raster for germany (2021)
+land_use <- rast(here(
+  "data",
+  "input",
+  "raster",
+  "classification_map_germany_2020_v02.tif"
+))
+st_crs(land_use)
+print(land_use)
+plot(land_use)
+
+# wind speed monthly average for January, April, July & October (2012)
+wind_speed_jan <- nc_open(here("data", "input", "raster", "FF_201201_monmean.nc"))
+wind_speed_april <- nc_open(here("data", "input", "raster", "FF_201204_monmean.nc"))
+wind_speed_july <- nc_open(here("data", "input", "raster", "FF_201207_monmean.nc"))
+wind_speed_oct <- nc_open(here("data", "input", "raster", "FF_201210_monmean.nc"))
+print(wind_speed_jan)
--- R/wrangle/wrangle.R
+++ R/wrangle/wrangle.R
@@ -1,3 +1,28 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b7ff9dd466dfd667bb0dad8a51f7b7c77baf3ffad34700f814e89cb02398ddd2
-size 796
+library(here)
+library(ggplot2)
+library(dplyr)
+
+# load data from data_read.R
+here("R/load/data_read.R") |> source()
+
+
+# filter and clean wind_points
+wind_points2 <- wind_points %>%
+  filter(
+    `Country/Area` == "Germany",
+    `Installation Type` == "Onshore",
+    `Status` %in% c("announced", "construction", "operating", "pre-construction")
+  )
+
+wind_points2 <- wind_points2 %>%
+  select()
+
+# create point layer out of wind farm data
+wind_points_sf <- st_as_sf(wind_points,
+                           coords = c("Longitude", "Latitude"),
+                           crs = "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
+str(wind_points_sf)
+ggplot() +
+  geom_sf(data = wind_points_sf) +
+  theme_minimal() +
+  labs(title = "Point Layer from Excel Data", x = "Longitude", y = "Latitude")
